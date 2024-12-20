46489d396c373574df3af05beb0a63e721a741b5
Saurabh Nishad <132405537+saurabhnishad23@users.noreply.github.com>
2024-12-20 15:05:09 +0530

    Update Pagination in ProductRepo.java

--- src/main/java/com/amaKart/amaKart_products/repositories/ProductRepo.java
+++ src/main/java/com/amaKart/amaKart_products/repositories/ProductRepo.java
@@ -1,7 +1,10 @@
 package com.amaKart.amaKart_products.repositories;
 
 import com.amaKart.amaKart_products.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
+  Page<Product> findAll (Pageable pageable);
 }
