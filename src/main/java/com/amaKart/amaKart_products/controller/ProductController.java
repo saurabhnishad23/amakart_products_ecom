82711ebecf0aa489dc69dac5564ff9603813df0c
Saurabh Nishad <132405537+saurabhnishad23@users.noreply.github.com>
2024-12-20 16:22:09 +0530

    Update Pagination to products page

--- src/main/java/com/amaKart/amaKart_products/controller/ProductController.java
+++ src/main/java/com/amaKart/amaKart_products/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.amaKart.amaKart_products.controller;
 import com.amaKart.amaKart_products.entities.Product;
 import com.amaKart.amaKart_products.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +15,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public Page<Product> getAllProducts(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "4") int size) {
+        return productService.getAllProducts(page, size);
     }
 
     @PostMapping
